e463680c3d1b6f878870b9b28c7a58dce0bea0ca
eshanwise <eshanwise@gmail.com>
2022-11-29 18:54:28 +0000

    final commit

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#author -Wiseman Ukasoanya
+
+def pow(a, b):
+
+    return (a ** b)
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+#author = Wiseman Ukasoanya
+
+def islower(c):
+
+    """Function checks for lowercase characters."""
+
+    if ord(c) >= 97 and ord(c) <= 122:
+
+        return True
+
+    else:
+
+        return False
